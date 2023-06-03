4cd28fa8f0b78ce7d8a79a7ffc7327acd4bc45d7
Ivan Rybakov <regformonly@yandex.ru>
2023-06-03 23:47:42 +0300

    new time value

--- 3.2_1.py
+++ 3.2_1.py
@@ -1,1 +1,8 @@
-assert abs(-42) == -42, 'Should be 42'
+#assert abs(-42) == -42, 'Should be 42'
+
+string = 'Привет, {}. меня зовут {}'.format('Иван', 'Игорь')
+print(string)
+
+name1, name2 = 'Антон', 'Владимир'
+string2 = f'Привет, {name1}. меня зовут {name2}'
+print(string2)
